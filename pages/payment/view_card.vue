4f9eb0b9a9ab6b3f50ba1b338c003ddbb752567f
Samson Weiner <samson.weiner@uconn.edu>
2018-11-14 13:15:32 -0500

    further updates to payment

--- pages/payment/view_card.vue
+++ pages/payment/view_card.vue
@@ -1,5 +1,12 @@
 <template>
-    <h1>View Card</h1>
+    <v-app id="tube">
+        <h1>View Card</h1>
+        <v-card width="280px" height="100px">
+            <p>More information About the Card</p>
+        </v-card>
+        <v-btn><span>Remove Card</span></v-btn>
+        <v-btn to="../account/payment"><span>Back</span></v-btn>
+    </v-app>
 </template>
 
 <script>
