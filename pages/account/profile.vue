b260272d2def67207d8dd0d25cfc0b1d04bd1ee6
Samson Weiner <samson.weiner@uconn.edu>
2019-02-27 12:02:52 -0500

    benchmark changes. Updated login page, forgot password now a button instead of link. Update password through profile.

--- pages/account/profile.vue
+++ pages/account/profile.vue
@@ -56,14 +56,87 @@
                 </v-tab>
                 <v-tab-item class="pt-2">
                     <v-card flat>
+                        <v-card-title primary-title>
+                            <h3 class="headline mb-0"> Change Password </h3>
+                        </v-card-title>
+                        <form @submit.prevent="resetPass">
+                            <v-layout>
+                                <v-flex>
+                                    <v-text-field
+                                        label="Enter Old Password"
+                                        name="oldpassword"
+                                        id="oldpassword"
+                                        v-model="oldpassword">
+                                    </v-text-field>
+                                </v-flex>
+                            </v-layout>
+                            <v-layout>
+                                <v-flex>
+                                    <v-text-field
+                                        label="Enter New Password"
+                                        name="newpassword"
+                                        id="newpassword"
+                                        v-model="newpassword">
+                                    </v-text-field>
+                                </v-flex>
+                            </v-layout>
+                            <v-layout>
+                                <v-flex>
+                                    <v-text-field
+                                        label="Confirm New Password"
+                                        name="confirmpassword"
+                                        id="confirmpassword"
+                                        v-model="confirmpassword">
+                                    </v-text-field>
+                                </v-flex>
+                            </v-layout>
+                            <v-layout>
+                                <v-flex>
+                                    <v-btn
+                                    :disabled="!passwordIsValid"
+                                    type="submit">
+                                    Submit
+                                    </v-btn>
+                                </v-flex>    
+                            </v-layout>  
+                        </form>
                     </v-card>
+                    <v-alert
+                        :value = passSuccess
+                        type="success">
+                    </v-alert>
+                    
                 </v-tab-item>
                 <v-tab ripple>
                     Email
                 </v-tab>
                 <v-tab-item class="pt-2">
                     <v-card flat>
-                        <h3>Hello</h3>
+                        <v-card-title primary-title>
+                            <h3 class="headline mb-0"> Change Email </h3>
+                        </v-card-title>
+                        <v-form @submit.prevent="changeUserName">
+                            <v-layout>
+                                <v-flex>
+                                    <v-text-field 
+                                        label="Enter Name"
+                                        name="name"
+                                        id="name"
+                                        v-model="name">
+                                    </v-text-field>
+                                </v-flex>
+                            </v-layout>
+                            <v-layout>
+                                <v-flex>
+                                    <v-btn 
+                                    :disabled="!nameIsValid"
+                                    type="submit"
+                                    >
+                                    Submit
+                                    </v-btn>
+                                </v-flex>
+                            </v-layout>
+                        </v-form>
                     </v-card>
                 </v-tab-item>
             </v-tabs>
@@ -73,7 +146,9 @@
 </template>
 
 <script>
-    import {user} from '~/store/index.js'
+    import { Auth } from '~/plugins/firebase-client-init.js'
+    import firebase from 'firebase/app'
+
 	export default {
 		name: "profile",
 		data: function() {
@@ -81,12 +156,52 @@
 		    return {
                 name: '',
                 email: '',
+                oldpassword: 'passwordtest01',
+                newpassword: 'passwordtest01',
+                confirmpassword: 'passwordtest01',
+                passSuccess: false,
+
             }
         },
         methods: {
-            changeUserName(name) {
-                console.log(this.$store.state.user.name);
+            changeUserName() {
+                var user = firebase.auth().currentUser;
+                console.log(this.user.name);
+                user.updateProfile({
+                    displayName: name,
+                }).then(function() {
+                // Update successful.
+                }).catch(function(error) {
+                // An error happened.
+                });
+                console.log(this.user.displayName);
+            },
+            resetPass() {
+                firebase.auth().currentUser.reauthenticateAndRetrieveDataWithCredential(
+                firebase.auth.EmailAuthProvider.credential(
+                    firebase.auth().currentUser.email, 
+                    this.oldpassword
+                    )
+                ).then(
+                    success => {
+                        var user = firebase.auth().currentUser;
+                        var newPassword = this.newpassword
+
+                        user.updatePassword(newPassword).then(
+                            success => {
+                                //console.log("success, new password:", this.newpassword)
+                                this.passSuccess = true;
+
+                        // Update successful.
+                        }).catch(function(error) {
+                        // An error happened.
+                        });
+                    }
+                ).catch(function(error) {
+
+                });
             }
+
         },
         computed: {
             user() {
@@ -94,7 +209,41 @@
             },
             nameIsValid() {
                 return this.name !== "";
-            }
+            },
+            passwordIsValid() {
+                //text fields empty
+                if (this.newpassword == '' && this.confirmpassword == '')
+                    return false;
+                //text fields do not match
+                if (this.newpassword !== this.confirmpassword)
+                    return false;
+                //else return true
+                else
+                    return true;
+                 
+                 //return this.password !== '' &&
+                        //this.confirmpassword !== '' &&
+                        //this.password == this.confirmpassword;
+       
+            },
+            //reAuthenticate() {
+                //var user = firebase.auth().currentUser;
+                //var credential = firebase.auth.EmailAuthProvider.credential(
+                 //   this.user.email,
+                //    oldpassword
+                //);
+
+                // Prompt the user to re-provide their sign-in credentials
+
+               // user.reauthenticateAndRetrieveDataWithCredential(credential).then(
+               //     success => {
+                //        return true 
+                //}).catch(function(error) {
+                //        return false
+                // An error happened.
+               // });
+
+            //}
 		},
 	}
 </script>
