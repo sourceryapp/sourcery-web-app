3ab7152221b63e6850c42bd774effca3f1729739
Brian Daley <382814+bdaley@users.noreply.github.com>
2019-11-26 19:50:24 -0500

    fixed layout and onboarding flow (#60)

--- pages/register.vue
+++ pages/register.vue
@@ -1,82 +1,78 @@
 <template>
-  <v-layout align-center justify-start column fill-height>
-    <v-card flat height="50%" width="18%" color="transparent">
-      <v-form @submit.prevent="registerValid">
-        <h1>Register</h1>
-        <v-alert v-if="message" :value="true" type="error" class="mt-2 mb-2">{{message}}</v-alert>
-        <v-text-field type="text" name="name" v-model="name" label="Full Name"></v-text-field>
-        <span class="red--text" v-for="(err, index) in errors.name" :key="index">{{err}}</span>
-        <v-text-field type="email" name="email" v-model="email" label="Email"></v-text-field>
-        <span class="red--text" v-for="(err, index) in errors.email" :key="index">{{err}}</span>
-        <v-layout>
-          <v-flex>
-            <v-text-field
-              type="password"
-              name="password"
-              v-model="password"
-              label="Password"
-              autocomplete="false"
-              hint="At least 8 characters and 1 special character."
-            ></v-text-field>
-          </v-flex>
-        </v-layout>
-        <v-layout>
-          <v-flex>
-            <v-text-field
-              type="password"
-              name="confirm_password"
-              v-model="confirm_password"
-              label="Confirm Password"
-            ></v-text-field>
-          </v-flex>
-        </v-layout>
-        <span class="red--text" v-for="(err, index) in errors.confirm_password" :key="index">{{err}}</span>
-        <v-layout>
-          <v-flex>
-            <v-text-field
-              type="phone"
-              name="phone"
-              v-model="phone"
-              label="Phone Number"
-            ></v-text-field>
-          </v-flex>
-        </v-layout>
-        <v-layout>
-          <v-flex align-center justify-center>
-            <v-btn type="submit" value="Register" color="primary">Next</v-btn>
-          </v-flex>
-        </v-layout>
-      </v-form>
-      <v-flex align-center justify-center>
-        <v-btn to="/login">Back</v-btn>
-      </v-flex>
+  <v-layout>
+      <v-flex xs12 sm6 offset-sm3>
+        <v-card flat color="transparent">
+        <v-form @submit.prevent="registerValid">
+            <h1>Register</h1>
+            <v-alert v-if="message" :value="true" type="error" class="mt-2 mb-2">{{message}}</v-alert>
+            <v-text-field type="text" name="name" v-model="name" label="Full Name"></v-text-field>
+            <span class="red--text" v-for="(err, index) in errors.name" :key="index">{{err}}</span>
+            <v-text-field type="email" name="email" v-model="email" label="Email" required></v-text-field>
+            <span class="red--text" v-for="(err, index) in errors.email" :key="index">{{err}}</span>
+            <v-layout>
+            <v-flex>
+                <v-text-field
+                type="password"
+                name="password"
+                v-model="password"
+                label="Password"
+                autocomplete="false"
+                hint="At least 8 characters and 1 special character."
+                ></v-text-field>
+            </v-flex>
+            </v-layout>
+            <v-layout>
+            <v-flex>
+                <v-text-field
+                type="password"
+                name="confirm_password"
+                v-model="confirm_password"
+                label="Confirm Password"
+                ></v-text-field>
+            </v-flex>
+            </v-layout>
+            <span class="red--text" v-for="(err, index) in errors.confirm_password" :key="index">{{err}}</span>
+            <v-layout>
+            <v-flex>
+                <v-text-field
+                type="phone"
+                name="phone"
+                v-model="phone"
+                label="Phone Number"
+                ></v-text-field>
+            </v-flex>
+            </v-layout>
+            <v-layout row align-center justify-center>
+                <v-btn to="/login">Back</v-btn>
+                <v-btn type="submit" value="Register" color="primary">Next</v-btn>
+            </v-layout>
+        </v-form>
+        <v-alert
+            :value = errorEmpty
+            type="warning">
+            <span color="white">All fields must be filled.</span>
+        </v-alert>
+        <v-alert
+            :value = errorMatch
+            type="warning">
+            <span color="white">Passwords do not match.</span>
+        </v-alert>
+        <v-alert
+            :value = errorLength
+            type="warning">
+            <span color="white">Password length must be atleast 8 characters.</span>
+        </v-alert>
+        <v-alert
+            :value = errorSpecial
+            type="warning">
+            <span color="white">Password must include a special character.</span>
+        </v-alert>
+
     </v-card>
+    </v-flex>
 
 
-    <v-alert
-        :value = errorEmpty
-        type="warning"
-        dismissible>
-        <span color="white">All fields must be filled.</span>
-    </v-alert>
-    <v-alert
-        :value = errorMatch
-        type="warning"
-        dismissible>
-        <span color="white">Passwords do not match.</span>
-    </v-alert>
-    <v-alert
-        :value = errorLength
-        type="warning"
-        dismissible>
-        <span color="white">Password length must be atleast 8 characters.</span>
-    </v-alert>
-    <v-alert
-        :value = errorSpecial
-        type="warning"
-        dismissible>
-        <span color="white">Password must include a special character.</span>
-    </v-alert>
+
 
   </v-layout>
 </template>
@@ -127,18 +123,18 @@ export default {
                 displayName: this.name,
                 photoURL: 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAGAAAABgCAQAAABIkb+zAAACOklEQVR4Ae3ZA2ydURwF8P9s2+bjPSdGo0aN08V+URtbL+a8BbO9xfZs2zaCuW7vbDx8uLfp/3dinw+XopRSSimllFJhYm9TjV08wwdoYB0f8ix2mDkTe0p7YIZxDeto/5I6rjHDxGtdkcc72n8H75CXruKn1CAcpi0cHE4NEv9kp+EubXHB3ew08QuH4hFt8cGj5Ajxx9hePE1bYi6k+4gvMJ+29GCe+CEzhvW0ZaQ+PVZ8wDW0ZWatuJfozrqyC9Qluotr2Sra8pOtEtewMkgBrBLXsC9QgX3iGm4EKnBDXOP7QAXeiWt4G6jAW3ENNwMVuCmu4UCgAgc6/DCqE1miO9+7X0oEgtVlF1gjPkiOKHs5Pbx9b2jme7SlxPmSC5we20v8kRjJh6Vt6jlU/JKZztsBj1XcH2zxGG3h4ERqkPgp0R35AhvMOuQT3cVnyRH/O9wt4zjLzaj00/F6/dfj9WrPj9eVUkqpRPeMMTnMxxbu4fWf5uP3uME93IZ5JpcxHi4lzGjWYgPPsom2cNDIs9jAWjNaXJvaw1RyES/SlpmLXGQqHb0Rgsv5hjaEvOJyIt6lWg4nacMNTppcHMu9LqYGL2ijCZ6bGuki0TEVuEIbbXDFVEgU2JsbaWPKRvYOf6C8SBtjLoY6yKbH4h5tvMHd5DgJR6Ivb9E6yK1EX6c3AMGDlRIcZtG6i5ktQWGpywJYKkHxgtMC5yUo1tM6TL0ERes2WkALaAEtEEm0gFJKKaWUUkp9ABvn3SEbw3cFAAAAAElFTkSuQmCC',
             }).then(() => {
-                
+
                 var userRef = db.collection('user-meta').doc(user.uid);
 
                 var setWithMerge = userRef.set({
                     phone: this.phone
                 }, { merge: true });
-  
-                this.$store.dispatch('signIn', {
+
+                this.$store.dispatch('auth/signIn', {
                     email: this.email,
                     password: this.password
                 }).then(() => {
-                    this.$router.replace('/')
+                    this.$router.push('/dashboard')
                 });
             })
 
