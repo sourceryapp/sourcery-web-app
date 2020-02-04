77f0ad8519158e34afe258fd3049e0eb0cfae506
Alex Mueller <alex.mueller392@gmail.com>
2020-02-04 14:21:52 -0500

    Feature/alert fixes (#126)
    * Moved alerts to relevant locations
    
    * Login page updates
    
    * Register page updates
    
    * removed validate function from login

--- pages/login.vue
+++ pages/login.vue
@@ -1,34 +1,52 @@
 <template>
 	<v-layout>
-        <v-flex  xs12 sm6 offset-sm3 md4 offset-md4>
+        <v-flex xs12 sm6 offset-sm3 md4 offset-md4>
             <v-form @submit.prevent="login">
                 <h1>Log In</h1>
-                <v-text-field type="email" name="email" v-model="email" placeholder="Email"></v-text-field>
+                <v-alert
+                    :value = loginError
+                    type="error"
+                    transition="slide-y-transition"
+                    outline>
+                    Username or password is incorrect.
+                </v-alert>
+                <v-text-field 
+                    type="email"
+                    name="email"
+                    v-model="email" 
+                    label="Email" 
+                    :rules="emailRules"
+                    required
+                    clearable
+                    validate-on-blur
+                    prepend-icon="email"
+                    autofocus
+                ></v-text-field>
                 <span class="text-red" v-for="(err, index) in errors.email" :key="index">{{err}}</span>
-                <v-text-field type="password" name="password" v-model="password" placeholder="Password"></v-text-field>
+                <v-text-field 
+                    name="password" 
+                    v-model="password" 
+                    label="Password"
+                    required
+                    :type="showPass ? 'text' : 'password'"
+                    :append-icon="showPass ? 'visibility' : 'visibility_off'"
+                    @click:append="showPass = !showPass"
+                    :rules="[v => !!v || 'Password is required']"
+                    prepend-icon="security"
+                    validate-on-blur
+                ></v-text-field>
                 <span class="text-red" v-for="(err, index) in errors.password" :key="index">{{err}}</span>
-
-                <v-btn type="submit" color="primary" :loading=loading>Log In</v-btn>
-
-                <v-divider class="mt-3 mb-3"></v-divider>
-
-                <h3>Forgot Your Password?</h3>
-                <v-btn to="/password">Reset Password</v-btn>
-
+                
+                <v-btn block type="submit" color="primary" :loading="loading" :disabled="loading">Log In</v-btn>
+                <v-btn block flat to="/password">Forgot password?</v-btn>
 
                 <v-divider class="mt-3 mb-3"></v-divider>
 
                 <h3>Don't Have an Account?</h3>
-                <v-btn to="/register">Register</v-btn>
+                <v-btn block outline color="primary" to="/register">Register</v-btn>
 
 
             </v-form>
-            <v-alert
-                :value = loginError
-                type="warning"
-                dismissible>
-                <span color="white">Invalid login, please try again.</span>
-            </v-alert>
         </v-flex>
 
 	</v-layout>
@@ -41,14 +59,21 @@
         layout: 'default',
 		data() {
 			return {
-				email: '',
-				password: '',
+                valid: true,
+                email: '',
+                emailRules: [
+                    v => !!v || 'Email is required',
+                    v => /.+@.+/.test(v) || 'Email must be valid',
+                ],
+                password: '',
+                showPass: false,
                 loginError: false,
                 loading: false,
 				errors: {
 					password: [],
 					email: []
-				},
+                },
+                errorMessage: ''
 			}
 		},
 		methods: {
