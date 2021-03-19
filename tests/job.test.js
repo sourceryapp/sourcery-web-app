c26f53f71537c40a1f5080c69621e6651d656940
Brian Daley <brian@uconn.edu>
2021-03-19 13:48:41 -0400

    firestore mocks work

--- tests/job.test.js
+++ tests/job.test.js
@@ -1,31 +1,51 @@
+// @url https://github.com/Upstatement/firestore-jest-mock
+import { FakeFirestore } from 'firestore-jest-mock'
+
+// Model that we want to test
 import { Job } from '../models/job'
-import { job } from './mocks/job'
 
-// Sample Job
-const validJob = new Job(job);
+// Fake record(s) for firestore
+import { readFileSync } from 'fs'
+const job = JSON.parse(readFileSync(`${__dirname}/mocks/job.json`))
 
+// Placeholder for the Job Object
+let testJob;
+
+beforeAll( async () => {
+    // Create a fake firestore db
+    const db = new FakeFirestore({
+        "requests": job
+    })
+
+    // Query the fake db for a request
+    let doc = await db.collection('requests').doc('04whT2LMmpL5JX7GpKQP').get()
+
+    // Wrap the doc with our Job class
+    testJob = new Job(doc);
+
+})
 
 describe('Job Model Tests', () => {
     test('property getters', () => {
-        expect(validJob.attachments).toContain(job.data().attachments[0])
-        expect(validJob.client_id).toEqual(job.data().client_id)
-        expect(validJob.created_at).toEqual(job.data().created_at)
-        expect(validJob.label).toEqual(job.data().label)
-        expect(validJob.pages).toEqual(job.data().pages)
-        expect(validJob.pricing.total).toEqual(job.data().pricing.total)
-        expect(validJob.repository.name).toEqual(job.data().repository.name)
-        expect(validJob.repository_id).toEqual(job.data().repository_id)
-        expect(validJob.status).toEqual(job.data().status)
+        expect(testJob.attachments).toContain(testJob.data().attachments[0])
+        expect(testJob.client_id).toEqual(testJob.data().client_id)
+        expect(testJob.created_at).toEqual(testJob.data().created_at)
+        expect(testJob.label).toEqual(testJob.data().label)
+        expect(testJob.pages).toEqual(testJob.data().pages)
+        expect(testJob.pricing.total).toEqual(testJob.data().pricing.total)
+        expect(testJob.repository.name).toEqual(testJob.data().repository.name)
+        expect(testJob.repository_id).toEqual(testJob.data().repository_id)
+        expect(testJob.status).toEqual(testJob.data().status)
     })
 
     test('methods return correct job status', () => {
-        expect(validJob.isArchived()).toEqual(false)
-        expect(validJob.isComplete()).toEqual(false)
-        expect(validJob.isDraft()).toEqual(false)
-        expect(validJob.isPending()).toEqual(false)
-        expect(validJob.isPickedUp()).toEqual(true)
-        expect(validJob.isReserved()).toEqual(false)
-        expect(validJob.prettyStatus()).toEqual('picked up')
+        expect(testJob.isArchived()).toEqual(false)
+        expect(testJob.isComplete()).toEqual(false)
+        expect(testJob.isDraft()).toEqual(false)
+        expect(testJob.isPending()).toEqual(false)
+        expect(testJob.isPickedUp()).toEqual(true)
+        expect(testJob.isReserved()).toEqual(false)
+        expect(testJob.prettyStatus()).toEqual('picked up')
 
     })
 
