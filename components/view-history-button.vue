8e2e021b6b5d6179384282fce42917b61b6e653f
Brian Kelleher <brian.kelleher@uconn.edu>
2022-01-04 10:02:11 -0700

    missed components

---
+++ components/view-history-button.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="text-center mt-8">
+    <v-btn color="grey darken-1" to="/request/history" text>
+      <v-icon left>
+        mdi-history
+      </v-icon>
+      View History
+    </v-btn>
+  </div>
+</template>
