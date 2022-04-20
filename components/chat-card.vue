550eddacdfdedfd46309e0d24b1339ba4e330d24
Brian Kelleher <brian.kelleher@uconn.edu>
2022-04-19 22:20:35 -0600

    better layout for back and forth messages

--- components/chat-card.vue
+++ components/chat-card.vue
@@ -2,19 +2,28 @@
   <div class="chat-card-viewport">
     <div class="chat-card-container">
       <v-card max-width="500" class="chat-card">
-        <v-list-item two-line>
+        <v-list-item two-line class="chat-card-title-bar">
           <v-list-item-content>
             <v-list-item-title class="text-h6">
-              Chat with an Archivist
+              {{ chatTitle }}
             </v-list-item-title>
-            <v-list-item-subtitle>Request #300 - UConn Archives &amp; Special Collections</v-list-item-subtitle>
+            <v-list-item-subtitle>{{ chatSubtitle }}</v-list-item-subtitle>
           </v-list-item-content>
           <v-list-item-action>
-            <v-btn icon @click="toggleOpen">
+            <v-btn icon @click="toggleMinimize">
               <v-icon>{{ icon }}</v-icon>
             </v-btn>
           </v-list-item-action>
         </v-list-item>
+        <v-card-text>
+          <div class="chat-card-messages">
+            <div v-for="message in messages" :key="message.id" :class="chatMessageClass(message)">
+              <div :class="chatMessageTextClass(message)">
+                {{ message.text }}
+              </div>
+            </div>
+          </div>
+        </v-card-text>
       </v-card>
     </div>
   </div>
@@ -24,20 +33,57 @@
 export default {
     data () {
         return {
-            open: true
+            open: true,
+            minimized: false,
+            request: {},
+            messages: [{
+                id: 1,
+                text: 'This is a message!',
+                user_id: 1
+            }, {
+                id: 2,
+                text: 'This is a response!',
+                user_id: 2
+            }]
         }
     },
     computed: {
         icon () {
-            if (this.open) {
+            if (this.minimized) {
                 return 'mdi-close'
             }
             return 'mdi-chevron-up'
+        },
+        chatTitle () {
+            return 'Chat with an Archivist'
+        },
+        chatSubtitle () {
+            if (this.request && this.request.id) {
+                return `Request ${this.request?.id} - ${this.request?.repository.name}`
+            }
+            return 'Request ### - Repository Name'
         }
     },
     methods: {
         toggleOpen () {
             this.open = !this.open
+        },
+        toggleMinimize () {
+            this.minimized = !this.minimized
+        },
+        chatMessageClass (message) {
+            let class_string = 'chat-card-message'
+            if (message.user_id !== 1) {
+                class_string += ' alt-right'
+            }
+            return class_string
+        },
+        chatMessageTextClass (message) {
+            let class_string = 'chat-card-message-text'
+            if (message.user_id !== 1) {
+                class_string += ' alt-purple'
+            }
+            return class_string
         }
     }
 }
@@ -65,7 +111,33 @@ export default {
         .chat-card {
             border-top-left-radius: 10px;
             border-top-right-radius: 10px;
-            background: linear-gradient(45deg, #654EA3, #431A5A);
+
+            .chat-card-title-bar {
+              background: linear-gradient(45deg, #654EA3, #431A5A);
+            }
+
+            .chat-card-message {
+              margin-bottom: 15px;
+              display: block;
+              display: flex;
+              justify-content: flex-start;
+              &.alt-right {
+                justify-content: flex-end;
+              }
+                .chat-card-message-text {
+                background-color: #555455;
+                color: white;
+                padding: 10px 15px;
+                border-radius: 15px;
+                max-width: 70%;
+                justify-self: left;
+                &.alt-purple {
+                  background-color: #654EA3;
+                  text-align: right;
+                }
+              }
+            }
+
         }
     }
 }
