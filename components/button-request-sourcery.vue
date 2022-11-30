45deae4acc7126109413e4d2e15c31e20d530619
Brian Kelleher <brian.kelleher@uconn.edu>
2022-11-30 09:43:44 -0700

    put in final link

--- components/button-request-sourcery.vue
+++ components/button-request-sourcery.vue
@@ -1,8 +1,8 @@
 <template>
-  <button :class="linkClass" :style="styleTagText">
+  <a :class="linkClass" :style="styleTagText" :href="buttonLink">
     <span :style="styleSpanTagText">Request with Sourcery</span>
     <svg height="28" :style="imageStyleTagText" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 277.01 294.33"><defs><style>.cls-1{ {{ svgPolygonStyle }} }</style></defs><polygon class="cls-1" points="206.1 116.36 277.01 92.86 205.96 70.49 182.6 0 160.36 70.62 89.74 92.86 160.23 116.22 182.6 187.27 206.1 116.36" /><polygon class="cls-1" points="85.81 223.42 138.1 206.08 85.71 189.59 68.48 137.6 52.08 189.69 0 206.08 51.98 223.32 68.48 275.71 85.81 223.42" /><polygon class="cls-1" points="225.96 260.34 259.95 249.07 225.89 238.35 214.69 204.57 204.04 238.41 170.19 249.07 203.97 260.27 214.69 294.33 225.96 260.34" /></svg>
-  </button>
+  </a>
 </template>
 
 <script>
@@ -19,7 +19,8 @@ export default {
     },
     data () {
         return {
-            svgHeight: '28'
+            svgHeight: '28',
+            buttonLink: 'https://sourceryapp.org'
         }
     },
     computed: {
@@ -67,7 +68,7 @@ export default {
             return c
         },
         markup () {
-            return `<a class="${this.linkClass}" style="${this.styleTagText}" href="https://sourceryapp.org/login"><span style="${this.styleSpanTagText}">Request with Sourcery</span> <svg height="${this.svgHeight}" style="${this.imageStyleTagText}" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 277.01 294.33"><defs><style>.cls-1{${this.svgPolygonStyle}}</style></defs><polygon class="cls-1" points="206.1 116.36 277.01 92.86 205.96 70.49 182.6 0 160.36 70.62 89.74 92.86 160.23 116.22 182.6 187.27 206.1 116.36" /><polygon class="cls-1" points="85.81 223.42 138.1 206.08 85.71 189.59 68.48 137.6 52.08 189.69 0 206.08 51.98 223.32 68.48 275.71 85.81 223.42" /><polygon class="cls-1" points="225.96 260.34 259.95 249.07 225.89 238.35 214.69 204.57 204.04 238.41 170.19 249.07 203.97 260.27 214.69 294.33 225.96 260.34" /></svg></a>`
+            return `<a class="${this.linkClass}" style="${this.styleTagText}" href="${this.buttonLink}"><span style="${this.styleSpanTagText}">Request with Sourcery</span> <svg height="${this.svgHeight}" style="${this.imageStyleTagText}" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 277.01 294.33"><defs><style>.cls-1{${this.svgPolygonStyle}}</style></defs><polygon class="cls-1" points="206.1 116.36 277.01 92.86 205.96 70.49 182.6 0 160.36 70.62 89.74 92.86 160.23 116.22 182.6 187.27 206.1 116.36" /><polygon class="cls-1" points="85.81 223.42 138.1 206.08 85.71 189.59 68.48 137.6 52.08 189.69 0 206.08 51.98 223.32 68.48 275.71 85.81 223.42" /><polygon class="cls-1" points="225.96 260.34 259.95 249.07 225.89 238.35 214.69 204.57 204.04 238.41 170.19 249.07 203.97 260.27 214.69 294.33 225.96 260.34" /></svg></a>`
         },
         hoverCSS () {
             if (this.dark) {
