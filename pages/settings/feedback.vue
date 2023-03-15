3aabbd68d7268ac3a9ffb231121da6e7e5c34593
Brian Kelleher <brian.kelleher@uconn.edu>
2023-03-15 15:45:45 -0600

    closes #353, swap join us link/support

--- pages/settings/feedback.vue
+++ pages/settings/feedback.vue
@@ -3,7 +3,7 @@
     <v-flex xs12 sm8 xl6 offset-sm2 offset-xl3>
       <sourcery-card title="Feedback" icon="mdi-comment-quote-outline" class="mt-12">
         <p class="p-4 mt-4">
-          Please email <a :href="carlyEmailLink">{{ carlyEmail }}</a> or <a :href="brianEmailLink">{{ brianEmail }}</a> for feedback regarding Sourcery!
+          Please email <a :href="emailLink">support@sourceryapp.org</a> for feedback regarding Sourcery!
         </p>
       </sourcery-card>
     </v-flex>
@@ -20,16 +20,12 @@ export default {
     data () {
         return {
             subject: 'Sourcery Feedback',
-            carlyEmail: 'carly@uconn.edu',
-            brianEmail: 'brian.kelleher@uconn.edu'
+            supportEmail: 'support@sourceryapp.org'
         }
     },
     computed: {
-        carlyEmailLink () {
-            return `mailto:${this.carlyEmail}?subject=${this.subject}`
-        },
-        brianEmailLink () {
-            return `mailto:${this.brianEmail}?subject=${this.subject}`
+        emailLink () {
+            return `mailto:${this.supportEmail}?subject=${this.subject}`
         }
     }
 }
