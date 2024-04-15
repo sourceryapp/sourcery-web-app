16233374db4092dd427973493877e17ef3e39817
Brian Kelleher <brian.kelleher@uconn.edu>
2024-04-15 18:45:22 -0400

    delete RLS folder in favor of supabase folder

--- rls/old_request_vendors.sql
+++
@@ -1,7 +0,0 @@
-(auth.uid() IN ( SELECT organizations.owner_id
-   FROM organizations
-  WHERE (organizations.id IN ( SELECT repositories.organization_id
-           FROM repositories
-          WHERE (repositories.id IN ( SELECT requests.repository_id
-                   FROM requests
-                  WHERE (requests.id = request_vendors.request_id)))))))
--- rpc/add_moddate.sql
+++
@@ -1,5 +0,0 @@
-create extension if not exists moddatetime schema extensions;
-
--- assuming the table name is "requests"
-create trigger handle_updated_at before update on requests 
-  for each row execute procedure moddatetime (updated_at);
