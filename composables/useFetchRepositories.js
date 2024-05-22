f37c2b464224ebe573affd4b2e7b52265dcde5fa
Brian Kelleher <brian.kelleher@uconn.edu>
2024-05-22 12:45:51 -0400

    restore a test account on dev environment to request to

--- composables/useFetchRepositories.js
+++ composables/useFetchRepositories.js
@@ -1,5 +1,6 @@
 export default function useFetchRepositories() {
     const supabase = useSupabaseClient()
+    const config = useRuntimeConfig()
 
     const repositories = useState('repositories', () => [])
     const fetchRepositoriesError = ref()
@@ -15,6 +16,20 @@ export default function useFetchRepositories() {
         `)
         .order('name')
         .eq('active', true)
+
+        if ( config.public.SOURCERY_ENV === 'development' ) {
+            const { data: devData, error: devError } = await supabase.from('repositories').select(`
+                *,
+                organization:organizations (*),
+                featured_image:featured_images (*)
+            `)
+            .order('name')
+            .eq('name', 'Sourcery U')
+            .single()
+            if ( devData ) {
+                data.push(devData)
+            }
+        }
 
         if ( error ) {
             fetchRepositoriesError.value = error
