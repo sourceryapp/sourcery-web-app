d0aa3d93b7f8472a9516dc8184908afd16d0ea70
Brian Kelleher <brian.kelleher@uconn.edu>
2024-07-09 16:14:19 -0400

    cleanup old stuff

--- components/requests/uri-alert.vue
+++
@@ -1,9 +0,0 @@
-<template>
-    <v-alert border="start" v-show="requestCount > 0" class="mb-4">You have {{ requestCount }} requests with unregistered organizations. You can <NuxtLink to="/requests/unregistered">view those here</NuxtLink>.</v-alert>
-</template>
-
-<script setup>
-const { requestCount, countUriRequests } = useFetchUriRequests()
-
-await countUriRequests()
-</script>
