fe27f4ec6d4422273133ddd578438df97e440d65
Brian Kelleher <brian.kelleher@uconn.edu>
2024-09-16 11:27:57 -0400

    delete old tests, update env to reflect new settings

--- tests/custom-commands/assertDisabled.js
+++
@@ -1,8 +0,0 @@
-exports.command = function (selector) {
-    this
-        .getAttribute(selector, 'disabled', function (result) {
-            // Is disabled
-            this.assert.equal(result.value, 'true', `${selector} is disabled.`)
-        })
-    return this
-}
--- tests/custom-commands/assertEnabled.js
+++
@@ -1,8 +0,0 @@
-exports.command = function (selector) {
-    this
-        .getAttribute(selector, 'disabled', function (result) {
-            // Not disabled
-            this.assert.equal(result.value, null, `${selector} is enabled.`)
-        })
-    return this
-}
