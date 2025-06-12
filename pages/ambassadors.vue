9e7312015165bd02db7691b20018991f3fba0ec7
Brian Kelleher <brian.kelleher@uconn.edu>
2025-06-12 11:04:38 -0400

    community & research ambassadors positions for NYC, remove washington, update banner

--- pages/ambassadors.vue
+++ pages/ambassadors.vue
@@ -1,42 +1,94 @@
 <template>
     <div id="page-ambassadors">
+
         <v-container>
-            <h1 class="text-h3 font-weight-medium text-center my-16">Community Ambassador Program</h1>
-            <p class="font-italic">Last Updated: April 1, 2025</p>
-            <p class="my-8 readable-text-width mx-auto">Sourcery, an open source, not-for-profit archival document delivery app based out of the University of Connecticut, invites applications for community ambassadors to act as local advocates for Sourcery and to help beta test the new version of the app. <strong>We are seeking current or recently graduated PhD/MLIS/MS/MA students based in the Washington, DC area</strong> who are passionate about digital humanities, archival research, and digital research tools. Sourcery is produced by the University of Connecticut's Greenhouse Studios in collaboration with <a class="text-primary" href="https://digitalscholar.org">Digital Scholar</a>, the same non-profit corporation behind <a class="text-primary" href="https://zotero.org">Zotero</a>, <a class="text-primary" href="https://tropy.org">Tropy</a>, and <a class="text-primary" href="https://omeka.org">Omeka</a>. Sourcery creates one place for researchers to submit requests for digital copies of archival documents and a place for paid researchers (called "Sourcerers") to fill them.</p>
 
-            <h2 class="mb-2">Responsibilities & Duties:</h2>
+            <v-tabs fixed-tabs v-model="tab">
+                <v-tab text="NYC Community Ambassador" value="nyc_community"></v-tab>
+                <v-tab text="NYC Research Ambassador" value="nyc_research"></v-tab>
+            </v-tabs>
 
-            <ul class="ml-5 mb-8">
-                <li>Act as a beta tester for Sourcery (complete requests and provide constructive feedback);</li>
-                <li>Attend virtual testing workshops, place and fulfill test requests in local archives, contribute to design and UX discussions, and participate in dialogue and provide other feedback as needed;</li>
-                <li>Introduce Sourcery to your research/social circles and act as a local point of contact for other "Sourcerers."</li>
-            </ul>
+            <v-tabs-window v-model="tab">
+                <v-tabs-window-item value="nyc_community" key="nyc_community">
+                    <h1 class="text-h3 font-weight-medium text-center my-16">Sourcery Community Ambassadors</h1>
+                    <p class="font-italic">Last Updated: June 10, 2025</p>
+                    <p class="my-8 readable-text-width mx-auto">Sourcery (<a href="https://sourceryapp.org">sourceryapp.org</a>), an open source, not-for-profit archival document delivery app based out of the University of Connecticut, invites applications for community ambassadors to act as local advocates for Sourcery and to help beta test the new version of the app. <strong>We are seeking current or recently graduated PhD/MLIS/MS/MA students based in the New York City area who are passionate about digital humanities, archival research, and digital research tools.</strong> Sourcery is produced by the University of Connecticut's Greenhouse Studios in collaboration with Digital Scholar (<a href="https://digitalscholar.org">digitalscholar.org</a>), the same non-profit corporation behind <a href="https://zotero.org">Zotero</a>, <a href="https://tropy.org">Tropy</a>, and <a href="https://omeka.org">Omeka</a>.Sourcery creates one place for researchers to submit requests for digital copies of archival documents and a place for paid researchers (called "Sourcerers") to fill them.</p>
 
-            <h2 class="mb-2">Qualified applicants will demonstrate the following:</h2>
-            <ul class="ml-5 mb-8">
-                <li>Must be a current or recently graduated PhD/MLIS/MS/MA student at a university in the  Washington, DC area;</li>
-                <li>Access to an iPhone or Android device running a recent version of the iOS or Android operating system;</li>
-                <li>Experience researching in closed archives;</li>
-                <li>Interest in the digital humanities.</li>
-            </ul>
+                    <h2 class="mb-2">Responsibilities & Duties:</h2>
 
-            <p>
-                <strong>Compensation is $1,000 for approximately 40 hours of work over a 6-week period this summer.</strong><br>
-            </p>
-    
-            <p>
-                Please send your CV/Resume and a statement (1-2 paragraphs) describing your interest in digital humanities and your experience in archival research to <a class="text-primary" href="mailto:amanda.breeden@uconn.edu">amanda.breeden@uconn.edu</a> by April 23, 2025.
-            </p>
+                    <ul class="ml-5 mb-8">
+                        <li>Act as a beta tester for Sourcery (complete requests and provide constructive feedback)</li>
+                        <li>Attend virtual testing workshops, place and fulfill test requests in local archives, contribute to design and UX discussions, and participate in dialogue and provide other feedback as needed</li>
+                        <li>Introduce Sourcery to your research/social circles and act as a local point of contact for other "Sourcerers"</li>
+                    </ul>
 
-            <v-row>
-                <v-col cols="12" md="6">
-                    <v-btn class="mt-8" block size="large" color="primary" href="mailto:amanda.breeden@uconn.edu" target="_blank">Send Application</v-btn>
-                </v-col>
-                <v-col cols="12" md="6">
-                    <v-btn class="mt-8" block size="large" color="secondary" href="/doc/Sourcery%20Ambassador.pdf" target="_blank">Download Details (PDF)</v-btn>
-                </v-col>
-            </v-row>
+                    <h2 class="mb-2">Qualified applicants will demonstrate the following:</h2>
+                    <ul class="ml-5 mb-8">
+                        <li>Must be a current or recently graduated PhD/MLIS/MS/MA student at a university in the New York City area</li>
+                        <li>Access to an iPhone or Android device running a recent version of the iOS or Android operating system</li>
+                        <li>Experience researching in closed archives</li>
+                        <li>Interest in the digital humanities</li>
+                    </ul>
+
+                    <p>
+                        <strong>Compensation is $1,000 for approximately 40 hours of work to begin July 1, 2025 and end July 31, 2025.</strong><br>
+                    </p>
+            
+                    <p>
+                        Please send your CV/Resume and a statement (1-2 paragraphs) describing your interest in digital humanities and your experience in archival research to <a href="mailto:amanda.breeden@uconn.edu?subject=NYC%20Community%20Ambassadors"><strong>amanda.breeden@uconn.edu</strong></a> by <strong>June 19, 2025</strong>. Interviews will be conducted remotely on June 23, 24, and 25.
+                    </p>
+
+                    <v-row>
+                        <v-col cols="12" md="6">
+                            <v-btn class="mt-8" block size="large" color="primary" href="mailto:amanda.breeden@uconn.edu?subject=NYC%20Community%20Ambassadors" target="_blank">Send Application</v-btn>
+                        </v-col>
+                        <v-col cols="12" md="6">
+                            <v-btn class="mt-8" block size="large" color="secondary" href="/doc/NYC%20Sourcery%20Community%20Ambassadors.pdf" target="_blank">Download Details (PDF)</v-btn>
+                        </v-col>
+                    </v-row>
+                </v-tabs-window-item>
+
+                <v-tabs-window-item value="nyc_research" key="nyc_research">
+                    <h1 class="text-h3 font-weight-medium text-center my-16">Sourcery Research Ambassadors</h1>
+                    <p class="font-italic">Last Updated: June 11, 2025</p>
+                    <p class="my-8 readable-text-width mx-auto"><a href="https://sourceryapp.org">Sourcery</a>, an open source, not-for-profit archival document delivery app based out of the University of Connecticut, invites applications for research ambassadors to help beta test the new version of the app and act as community advocates for Sourcery.</p>
+
+                    <p class="my-8 readable-text-width mx-auto">We are seeking active humanities scholars who are comfortable using digital research tools and who need archival documents retrieved from repositories in New York City. Sourcery is produced by the University of Connecticut’s Greenhouse Studios in collaboration with <a href="https://digitalscholar.org">Digital Scholar</a>, the same non-profit corporation behind <a href="https://zotero.org">Zotero</a>, <a href="https://tropy.org">Tropy</a>, and <a href="https://omeka.org">Omeka</a>. Sourcery creates one place for researchers to submit requests for digital reference scans of archival documents and a place for paid researchers (called "Sourcerers") to fill them.</p>
+
+                    <h2 class="mb-2">Responsibilities & Duties:</h2>
+
+                    <ul class="ml-5 mb-8">
+                        <li>Act as a beta tester for Sourcery (submit document retrieval requests for archival items located in New York City-area repositories and provide constructive feedback)</li>
+                        <li>Attend virtual design and UX discussions and provide other feedback as needed</li>
+                        <li>Introduce Sourcery to your research/social circles</li>
+                    </ul>
+
+                    <h2 class="mb-2">Qualified applicants will demonstrate the following:</h2>
+                    <ul class="ml-5 mb-8">
+                        <li>Must be a postdoctoral or ABD scholar of history, literature, or related field who needs primary source documents retrieved from repositories in the New York City metro area</li>
+                        <li>Access to an iPhone or Android device running a recent version of the iOS or Android operating system</li>
+                        <li>Experience researching in closed archives</li>
+                        <li>Experience using digital research tools</li>
+                    </ul>
+
+                    <p>
+                        <strong>Compensation is $500 for approximately 8 hours of work to begin July 7, 2025 and be completed by August 1, 2025. In addition, you will receive scans of the archival items you request.</strong><br>
+                    </p>
+            
+                    <p>
+                        Please send your CV/Resume and a statement (1-2 paragraphs) describing your archival document needs and your experience using digital research tools to <a href="mailto:amanda.breeden@uconn.edu?subject=NYC%20Research%20Ambassadors">amanda.breeden@uconn.edu</a> by June 22, 2025. Interviews will be conducted remotely on June 26 and June 27.
+                    </p>
+
+                    <v-row>
+                        <v-col cols="12" md="6">
+                            <v-btn class="mt-8" block size="large" color="primary" href="mailto:amanda.breeden@uconn.edu?subject=NYC%20Research%20Ambassadors" target="_blank">Send Application</v-btn>
+                        </v-col>
+                        <v-col cols="12" md="6">
+                            <v-btn class="mt-8" block size="large" color="secondary" href="/doc/Sourcery%20Research%20Ambassadors_06112025.pdf" target="_blank">Download Details (PDF)</v-btn>
+                        </v-col>
+                    </v-row>
+                </v-tabs-window-item>
+            </v-tabs-window>
         </v-container>
     </div>
 </template>
@@ -45,4 +97,19 @@
 definePageMeta({
     layout: 'public'
 })
+const route = useRoute();
+const tab = ref(null);
+
+onMounted(() => {
+    if (route.query.tab) {
+        tab.value = route.query.tab;
+    } else {
+        tab.value = 'nyc_community'; // Default to the first tab
+    }
+});
+
+watch(tab, (newTab) => {
+    const query = { ...route.query, tab: newTab };
+    navigateTo({ path: route.path, query }, { replace: true });
+});
 </script>
